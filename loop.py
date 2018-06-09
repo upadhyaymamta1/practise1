c4023f72a30c1c9cfea86b4e47709822591460c9
upadhyaymamta1 <mamtaupadhyay1@gmail.com>
2018-06-09 18:43:38 +0530

    confusion

---
+++ loop.py
@@ -0,0 +1,24 @@
+def loop():
+    for x in range (1,10):
+        print(x)
+
+def table():
+    for x in range (2,22,2):
+        print (x)
+
+def table1():
+    for x in range (3,33,3):
+        print(x)
+
+def add(a,b,c):
+    c=a+b
+    print ('c', a+b)
+
+def intrest1(p,r,t):
+    p=1000 and r=10 and t=5:
+        i = p*r*t/100
+        print("intrest ="i )
+       
+
+    
+    
